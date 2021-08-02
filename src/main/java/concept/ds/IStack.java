ec5f403037ad0d36fac2231c4ac4addc4463dd3a
KumarDara <dkumarswamy@gmail.com>
2021-08-02 21:51:34 +0530

    Update IStack.java

--- src/main/java/concept/ds/IStack.java
+++ src/main/java/concept/ds/IStack.java
@@ -4,7 +4,7 @@ public interface IStack<E> {
   public E push(E ele);
   public E pop();
   public E peek();
-  public boolean empty();
+  public boolean isEmpty();
   public int size();
   public int search(E ele);
 }
